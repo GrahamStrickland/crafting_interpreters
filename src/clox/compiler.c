9875f037d38217557e95f206496124ee0b6b5d4e
Graham Strickland <grahamhstrickland@gmail.com>
2025-02-18 18:59:27 +0200

    Add stack frames to clox interpreter

--- src/clox/compiler.c
+++ src/clox/compiler.c
@@ -661,7 +661,7 @@ static void statement() {
   }
 }
 
-bool compile(const char *source) {
+ObjFunction *compile(const char *source) {
   initScanner(source);
   Compiler compiler;
   initCompiler(&compiler, TYPE_SCRIPT);
