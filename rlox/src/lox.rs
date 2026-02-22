ceeb206b889d6d728e2d9c1a38174ae20623d4f2
Graham Strickland <grahamhstrickland@gmail.com>
2026-02-22 08:27:58 +0200

    Extract LoxState into Lox struct with methods in rlox interpreter

---
+++ rlox/src/lox.rs
@@ -0,0 +1,70 @@
+use std::fs::File;
+use std::io::{BufReader, Read, Result, stdin};
+
+pub struct Lox {
+    pub had_error: bool,
+}
+
+impl Lox {
+    pub fn new() -> Self {
+        Lox { had_error: false }
+    }
+
+    pub fn run_file(self, path: &mut Option<String>) -> Result<()> {
+        match path {
+            Some(p) => {
+                let file = File::open(p)?;
+                let mut buf_reader = BufReader::new(file);
+                let mut contents = String::new();
+                buf_reader.read_to_string(&mut contents)?;
+                self.run(contents);
+
+                if self.had_error {
+                    std::process::exit(0x01000001)
+                }
+
+                Ok(())
+            }
+            None => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid path,\nUsage: rlox [script]",
+            )),
+        }
+    }
+
+    pub fn run_prompt(mut self) -> Result<()> {
+        use std::io::{self, Write};
+
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap();
+            let mut line = String::new();
+            match stdin().read_line(&mut line) {
+                Ok(_) => {
+                    self.run(line);
+                    self.had_error = false;
+                }
+                Err(_) => break,
+            }
+        }
+        Ok(())
+    }
+
+    pub fn run(&self, source: String) {
+        let scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens();
+
+        for token in tokens {
+            println!("{}", token);
+        }
+    }
+
+    pub fn error(self, line: i32, message: String) {
+        self.report(line, String::new(), message);
+    }
+
+    pub fn report(mut self, line: i32, location: String, message: String) {
+        eprintln!("[line {}] Error{}: {}", line, location, message);
+        self.had_error = true;
+    }
+}
--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,28 +1,15 @@
+use rlox::lox::Lox;
 use std::env::args;
-use std::fs::File;
-use std::io::{BufReader, Read, Result, stdin};
-
-static mut HAD_ERROR: bool = false;
-
-struct LoxState {
-    pub had_error: bool,
-}
-
-impl LoxState {
-    pub fn new() -> Self {
-        LoxState { had_error: false }
-    }
-}
 
 fn main() {
     let mut args = args();
-    let mut state = LoxState::new();
+    let lox = Lox::new();
 
     if args.len() > 2 {
         println!("Usage: rlox [script]");
         std::process::exit(0x01000000)
     } else if args.len() == 2 {
-        match run_file(&mut state, &mut args.nth(1)) {
+        match lox.run_file(&mut args.nth(1)) {
             Ok(()) => std::process::exit(0x0),
             Err(err) => {
                 eprintln!("{}", err);
@@ -30,7 +17,7 @@ fn main() {
             }
         }
     } else {
-        match run_prompt(&mut state) {
+        match lox.run_prompt() {
             Ok(()) => std::process::exit(0x0),
             Err(err) => {
                 eprintln!("{}", err);
@@ -38,62 +25,4 @@ fn main() {
             }
         }
     }
-}
-
-fn run_file(state: &mut LoxState, path: &mut Option<String>) -> Result<()> {
-    match path {
-        Some(p) => {
-            let file = File::open(p)?;
-            let mut buf_reader = BufReader::new(file);
-            let mut contents = String::new();
-            buf_reader.read_to_string(&mut contents)?;
-            run(state, contents);
-
-            if state.had_error {
-                std::process::exit(0x01000001)
-            }
-
-            Ok(())
-        }
-        None => Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "Invalid path,\nUsage: rlox [script]",
-        )),
-    }
-}
-
-fn run_prompt(state: &mut LoxState) -> Result<()> {
-    use std::io::{self, Write};
-
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-        let mut line = String::new();
-        match stdin().read_line(&mut line) {
-            Ok(_) => {
-                run(state, line);
-                state.had_error = false;
-            }
-            Err(_) => break,
-        }
-    }
-    Ok(())
-}
-
-fn run(state: &mut LoxState, source: String) {
-    let scanner = Scanner::new(source);
-    let tokens = scanner.scan_tokens();
-
-    for token in tokens {
-        println!("{}", token);
-    }
-}
-
-fn error(state: &mut LoxState, line: i32, message: String) {
-    report(state, line, String::new(), message);
-}
-
-fn report(state: &mut LoxState, line: i32, location: String, message: String) {
-    eprintln!("[line {}] Error{}: {}", line, location, message);
-    state.had_error = true;
 }
