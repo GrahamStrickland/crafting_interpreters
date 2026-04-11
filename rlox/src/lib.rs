7e09c2764334e2316b4581e3b1e61565a08c8065
Graham Strickland <grahamhstrickland@gmail.com>
2026-04-11 15:49:15 +0200

    Add tests for TokenType to rlox interpreter

--- rlox/src/lib.rs
+++ rlox/src/lib.rs
@@ -3,3 +3,45 @@ pub mod lox;
 pub mod scanner;
 pub mod token;
 pub mod token_type;
+
+#[cfg(test)]
+mod tests {
+    use super::literal::Literal;
+    use super::token::Token;
+    use super::token_type::TokenType;
+
+    #[test]
+    fn test_token_format() {
+        let token = Token::new(
+            TokenType::Number,
+            String::from("1.234"),
+            Literal::Double(1.234),
+            1,
+        );
+        assert_eq!(
+            format!("{:?}", token),
+            String::from(
+                "Token { token_type: Number, lexeme: \"1.234\", literal: Double(1.234), line: 1 }"
+            )
+        );
+
+        let token = Token::new(
+            TokenType::String,
+            String::from("\"hello\""),
+            Literal::String(String::from("hello")),
+            1,
+        );
+        assert_eq!(
+            format!("{:?}", token),
+            String::from(
+                "Token { token_type: String, lexeme: \"\\\"hello\\\"\", literal: String(\"hello\"), line: 1 }"
+            )
+        );
+
+        let token = Token::new(TokenType::And, String::from("and"), Literal::Null, 1);
+        assert_eq!(
+            format!("{:?}", token),
+            String::from("Token { token_type: And, lexeme: \"and\", literal: Null, line: 1 }")
+        );
+    }
+}
