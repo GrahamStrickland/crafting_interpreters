7df26a67d2b3eb10e2796c391f4322afa679a9f6
Graham Strickland <grahamhstrickland@gmail.com>
2026-04-18 09:14:10 +0200

    Move token tests from lib.rs to token.rs

--- rlox/src/token.rs
+++ rlox/src/token.rs
@@ -30,3 +30,45 @@ impl fmt::Display for Token {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Token;
+    use super::TokenType;
+    use crate::literal::Literal;
+
+    #[test]
+    fn test_token_format() {
+        let token = Token::new(
+            TokenType::Number,
+            String::from("1.234"),
+            Literal::Double(1.234),
+            1,
+        );
+        assert_eq!(
+            format!("{:?}", token),
+            String::from(
+                "Token { token_type: Number, lexeme: \"1.234\", literal: Double(1.234), line: 1 }"
+            )
+        );
+
+        let token = Token::new(
+            TokenType::String,
+            String::from("\"hello\""),
+            Literal::String(String::from("hello")),
+            1,
+        );
+        assert_eq!(
+            format!("{:?}", token),
+            String::from(
+                "Token { token_type: String, lexeme: \"\\\"hello\\\"\", literal: String(\"hello\"), line: 1 }"
+            )
+        );
+
+        let token = Token::new(TokenType::And, String::from("and"), Literal::Null, 1);
+        assert_eq!(
+            format!("{:?}", token),
+            String::from("Token { token_type: And, lexeme: \"and\", literal: Null, line: 1 }")
+        );
+    }
+}
